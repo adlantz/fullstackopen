4a861999758d710105d004b1027d59c8fdfb0a26
adlantz <asher.lantz96@gmail.com>
2024-01-22 10:56:10 -0700

    problems 2.16-2.17

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 
 const Filter = ({ handleFilterChange, filter }) => <div>Filter: <input value={filter} onChange={handleFilterChange} /></div>
@@ -21,13 +21,15 @@ const PersonForm = ({ addPerson, handleNameChange, newName, handleNumberChange, 
 }
 
 
-const Persons = ({ persons, filter }) => {
+
+
+const Persons = ({ persons, filter, removePerson }) => {
   return (
     <ul>
       {persons.reduce(
         (list_array, person) => {
           if (person.name.toLowerCase().includes(filter.toLowerCase())) {
-            list_array.push(<li key={person.name}>{person.name} {person.number}</li>)
+            list_array.push(<li key={person.id}>{person.name} {person.number} <button onClick={() => removePerson(person.id, person.name)}>delete</button></li>)
           }
           return list_array
         },
@@ -35,6 +37,31 @@ const Persons = ({ persons, filter }) => {
       )
       }
     </ul>
+  )
+}
+
+const SuccessNotification = ({ message, isError }) => {
+
+
+
+  if (!message) {
+    return null
+  }
+
+  const successStyle = {
+    color: isError ? 'red' : 'green',
+    background: 'lightgrey',
+    fontStyle: 'solid',
+    fontSize: 20,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+
+  return (
+    <div style={successStyle}>
+      {message}
+    </div>
   )
 }
 
@@ -48,14 +75,21 @@ const App = () => {
 
   const [filter, setFilter] = useState('')
 
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [isError, setIsError] = useState(false)
+
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    personService.getAll().then(initialPersons => setPersons(initialPersons))
   }, [])
+
+  const removePerson = (id, name) => {
+    if (window.confirm(`Are you sure you want to delete ${name}?`)) {
+      personService.remove(id).then(deletedPerson => {
+        setPersons(persons.filter(person => person.id !== deletedPerson.id))
+      })
+    }
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -72,17 +106,43 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.reduce((found, person) => found || person.name === newName, false)) {
-      alert(`${newName} is already added to phonebook!`)
+    const existingPerson = persons.find(person => person.name === newName)
+
+    if (existingPerson) {
+      const personObj = { ...existingPerson, number: newNumber }
+      if (window.confirm(`${newName} is already in the phonebook. Would you like to update their number?`)) {
+        personService.update(existingPerson.id, personObj).then(updatedPerson => {
+          setPersons(persons.map(person => person.id === updatedPerson.id ? updatedPerson : person))
+        }).catch(error => {
+          setIsError(true)
+          setSuccessMessage(`Information of ${existingPerson.name} has been removed from phonebook.`)
+          setTimeout(() => {
+            setSuccessMessage(null)
+          }, 5000)
+
+        })
+        setNewName("")
+        setNewNumber("")
+      }
     }
     else {
       const personObj = {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObj))
-      setNewName("")
-      setNewNumber("")
+      personService.create(personObj).then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setIsError(false)
+        setSuccessMessage(`Successfully added ${returnedPerson.name} to phonebook!`)
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000)
+        setNewName("")
+        setNewNumber("")
+      }
+      )
+
+
     }
 
   }
@@ -90,11 +150,12 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+      <SuccessNotification message={successMessage} isError={isError} />
       <Filter handleFilterChange={handleFilterChange} filter={filter} />
       <h2>Add new contact</h2>
       <PersonForm addPerson={addPerson} handleNameChange={handleNameChange} newName={newName} handleNumberChange={handleNumberChange} newNumber={newNumber} />
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} removePerson={removePerson} />
     </div>
   )
 }
